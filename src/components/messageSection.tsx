e124228e6c761bd1dbc54665051e0c452ece7600
amitsaini144 <1amit24saini5@gmail.com>
2024-07-26 10:12:12 +0530

    updated bg color

--- src/components/messageSection.tsx
+++ src/components/messageSection.tsx
@@ -15,7 +15,7 @@ export default function MessageSection({ recivedMessages, messagesEndRef, curren
                     const isCurrentUser = message.sender === currentUser.username;
                     return (
                         <div key={index} className={`mb-2 flex ${isCurrentUser ? 'justify-end' : 'justify-start'}`}>
-                            <div className={`max-w-[70%] rounded-b-2xl px-4 py-2 ${isCurrentUser ? 'bg-[#03AED2] text-white rounded-l-2xl' : 'bg-white text-black rounded-r-2xl'}`}>
+                            <div className={`max-w-[70%] rounded-b-2xl px-5 py-2 ${isCurrentUser ? 'bg-[#03AED2] text-white rounded-l-2xl' : 'bg-[#DDDDDD] text-black rounded-r-2xl'}`}>
                                 {!isCurrentUser && <div className="font-bold">{message.sender}</div>}
                                 <div>{message.content}</div>
                             </div>
